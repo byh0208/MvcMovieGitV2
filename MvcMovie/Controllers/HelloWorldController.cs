0b7f577eb6e8fdd2e8a385deb16bbce95e39f38c
baiyinghui <baiyinghui1314@outlook.com>
2019-08-14 21:06:55 +0800

    【提交类型：】创建项目，搭建.net Core开发环境

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "This is my default action……";
+        //}
+        [HttpPost]
+        public string Welcome()
+        {
+            return "This is the Welcome action method";
+        }
+        public IActionResult Welcome(string name,int NumTimes = 1)
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name},ID = {ID}");
+            ViewData["message"] = "Hello:" + name;
+            ViewData["NumTimes"] = NumTimes;
+
+            return View();
+        }
+    }
+}
